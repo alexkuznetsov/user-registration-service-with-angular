91e6c775d371a7da1a9d57311659f1cf775d89f6
Kuznetsov Alexander <kuznetsov@dm.findexit.kz>
2024-07-10 16:29:38 +0500

    added user location model with id

---
+++ src/LoginPage.Domain/UserLocations/UserLocation.cs
@@ -0,0 +1,27 @@
+﻿using LoginPage.Domain.Common;
+using LoginPage.Domain.Locations;
+using LoginPage.Domain.Users;
+
+namespace LoginPage.Domain.UserLocations;
+
+public class UserLocation : AggregateRoot<UserLocationId, Guid>
+{
+    public DateTime CreatedAt { get; }
+    public ProvinceId ProvinceId { get; }
+
+    public UserId UserId { get; }
+
+    private UserLocation(UserLocationId id, DateTime createdAt, UserId userId, ProvinceId provinceId) :
+        base(id)
+    {
+        CreatedAt = createdAt;
+        UserId = userId;
+        ProvinceId = provinceId;
+    }
+
+    public static UserLocation Create(UserId userId, ProvinceId provinceId)
+    {
+        return new(UserLocationId.CreateUnique(), DateTime.UtcNow, userId, provinceId);
+    }
+}
+
---
+++ src/LoginPage.Domain/UserLocations/UserLocationId.cs
@@ -0,0 +1,28 @@
+using LoginPage.Domain.Common;
+
+namespace LoginPage.Domain.UserLocations;
+
+public class UserLocationId : AggregateRootId<Guid>
+{
+    public override Guid Value { get; protected set; }
+
+    private UserLocationId(Guid value)
+    {
+        Value = value;
+    }
+
+    public static UserLocationId CreateUnique()
+    {
+        return new(Guid.NewGuid());
+    }
+
+    public static UserLocationId Create(Guid value)
+    {
+        return new(value);
+    }
+
+    public override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Value;
+    }
+}
